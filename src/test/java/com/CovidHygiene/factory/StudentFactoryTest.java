3bfa92ce55bb3411937e77caa023fae5d5ef622d
216279585 <216279585@mycput.ac.za>
2020-07-06 08:31:04 +0000

    Added test for StudentFactory

---
+++ src/test/java/com/CovidHygiene/factory/StudentFactoryTest.java
@@ -0,0 +1,33 @@
+package com.CovidHygiene.factory;
+
+import com.CovidHygiene.entity.Student;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentFactoryTest {
+
+
+    @Test
+    public void createStudent() {
+
+        Student student = StudentFactory.createStudent("Damien", "Mally", "Cape Town, South Africa, Earth");
+
+        assertNotNull(student);
+
+        System.out.println(student.toString());
+    }
+
+
+    @Test
+    public void studentNumberCheck() {
+
+        Student student = StudentFactory.createStudent("Damien", "Mally", "Cape Town, South Africa, Earth");
+
+        assertNotNull(student.getStudentNum());
+
+        System.out.println(student.getStudentNum());
+    }
+
+
+}
