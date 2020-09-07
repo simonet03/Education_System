e63441721fdc8fbdec985bf8609e3c444c48d2d3
Ethan <ethanpetersen10@gmail.com>
2020-09-07 11:38:13 +0200

    Changed Folder Structure

--- src/main/java/com/CovidHygiene/service/user/impl/StockServiceImpl.java
+++ src/main/java/com/CovidHygiene/service/user/impl/StockServiceImpl.java
@@ -1,4 +1,4 @@
-package com.CovidHygiene.service.impl;
+package com.CovidHygiene.service.user.impl;
 
 import com.CovidHygiene.entity.Stock;
 import com.CovidHygiene.repository.user.StockRepository;
